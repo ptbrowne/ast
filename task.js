ed9ef2da4e584f4873f79ab998395557c0980f28
Huge Huang <huge@spotify.com>
2013-01-07 21:25:22 -0500

    added doc

--- task.js
+++ task.js
@@ -3,10 +3,20 @@
 var fs = require('fs');
 var codegen = require('escodegen');
 
+/**
+ * @constructor
+ * @class Task
+ */
 function Task(name) {
   this.name = name;
 }
 
+/**
+ * @method execute
+ * @interface
+ * @description This method should be not invoked directly
+ *              It's designed to be implemented in derived class
+ */
 Task.prototype.execute = function() {
   return new Error("should be implemented in derived class");
 };
