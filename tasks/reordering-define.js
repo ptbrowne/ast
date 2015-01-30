13fc70f3d9fe85babe65d0f183ec0b96e1747d77
Patrick Browne <pt.browne@gmail.com>
2015-01-30 14:40:54 +0100

    refactor

--- tasks/reordering-define.js
+++ tasks/reordering-define.js
@@ -6,39 +6,30 @@ var estraverse = require('estraverse');
 var _ = require('underscore');
 var console = require('console');
 
-var reorder = function (arr, indices) {
-  var res = [];
-  for (var i = 0; i < indices.length; i++) {
-    res.push(arr[indices[i]]);
-  }
-  return res;
+var enumerate = function (arr) {
+  return _.map(arr, function (v,k) { return [k,v]; });
 };
 
-var _priorities = _.map([
+var _priorities = enumerate([
   'app',
   'marionette',
   'underscore',
   'views/',
   'models/'
-], function (v,k) { return [k,v]; });
+]);
 
 var priority = function (name) {
   var found = _.find(_priorities, function (n) {
-    var rx = new RegExp(n[1]);
-    var test = rx.test(name);
-    return test;
+    return new RegExp(n[1]).test(name);
   });
   return found ? found[0] : Infinity;
 };
 
-var annotate = function (arr, fn) {
-  return _.map(arr, function (n) { return [n, fn(n)]; });
-};
-
-var getOrder = function (names) {
-  var priorities = _.pluck(_.sortBy(annotate(names, priority), 1), 0);
-  var order = _.map(priorities, function (n) { return names.indexOf(n); });
-  return order;
+var multiSort = function (arrs, fn) {
+  var zipped = _.zip.apply(_, arrs);
+  var sorted = _.sortBy(zipped, function (d) { return fn.apply(null, d); });
+  var unzipped = _.zip.apply(_, sorted);
+  return unzipped;
 };
 
 task.execute = function() {
@@ -51,11 +42,11 @@ task.execute = function() {
         var fn = node.expression.arguments[1];
         var nbParams = fn.params.length;
         var arr = node.expression.arguments[0];
-        var elems = arr.elements.slice(0, nbParams);
+        var named_deps = arr.elements.slice(0, nbParams);
         var rest = arr.elements.slice(nbParams);
-        var order = getOrder(_.pluck(elems, 'value'));
-        arr.elements = reorder(elems, order).concat(rest);
-        fn.params = reorder(fn.params, order);
+        var sorted = multiSort([named_deps, fn.params], function (dep, param) { return priority(dep.value); });
+        arr.elements = sorted[0].concat(rest);
+        fn.params = sorted[1];
       }
     }
   });
